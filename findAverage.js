6ac157412fa33fb84882dd38d5b28bef61e99f63
S-Spiegl <steven.spiegl@gmail.com>
2022-09-19 00:00:05 +0100

    refactored to ternary

--- findAverage.js
+++ findAverage.js
@@ -1,10 +1,5 @@
 function findAverage(array) {
-  if(array.length > 0){
-    return (array.reduce((prev, curr) => prev + curr)/array.length)
-  }
-  else{
-    return 0
-  }
+  return array.length > 0 ? (array.reduce((prev, curr) => prev + curr)/array.length) : 0
 }
 
 module.exports = findAverage;
