d43ab6bf3309c32c9d73d7ee8da792da8b1f219d
S-Spiegl <steven.spiegl@gmail.com>
2022-09-18 23:58:07 +0100

    works for floats

--- findAverage.test.js
+++ findAverage.test.js
@@ -4,4 +4,7 @@ describe('findAverage', () => {
   it('calculates the average of an input', () => {
     expect(findAverage([1,1,1])).toEqual(1)
   })
+  it('calculates can return a float', () => {
+    expect(findAverage([1,2,3,4])).toEqual(2.5)
+  })
 })
